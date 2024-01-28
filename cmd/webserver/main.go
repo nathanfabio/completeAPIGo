105090586036ed8c9075daf59d8e2ee5a2264aff
Nathan Fabio <nathan_3_m@hotmail.com>
2024-01-28 15:30:31 -0300

    feat: update main.go

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"net/http"
 
+	"github.com/go-chi/chi"
+	"github.com/nathanfabio/completeAPIGo/config/env"
 	"github.com/nathanfabio/completeAPIGo/config/logger"
+	"github.com/nathanfabio/completeAPIGo/internal/database"
+	"github.com/nathanfabio/completeAPIGo/internal/database/sqlc"
+	"github.com/nathanfabio/completeAPIGo/internal/handler/routes"
+	"github.com/nathanfabio/completeAPIGo/internal/handler/userhandler"
+	"github.com/nathanfabio/completeAPIGo/internal/repository/userepository"
+	"github.com/nathanfabio/completeAPIGo/internal/service/userservice"
 )
 
 func main() {
 	logger.InitLogger()
-
-	user := user{
-		Name:     "nathan",
-		Age:      24,
-		Password: "1234",
-	}
-	
 	slog.Info("Starting API")
-	slog.Info("Creating a user", "user", user.LogUser())
+
+	_, err := env.LoadingConfig(".")
+	if err != nil {
+		slog.Error("failedto load enviroment variables", err, slog.String("package", "main"))
+		return
+	}
+
+	dbConnection, err := database.NewDBConnection()
+	if err != nil {
+		slog.Error("error to connect to database", "err", err, slog.String("package", "main"))
+		return
+	}
+
+	router := chi.NewRouter()
+	queries := sqlc.New(dbConnection)
+
+
+	userRepo := userepository.NewUserRepository(dbConnection, queries)
+	newUserService := userservice.NewUserService(userRepo)
+	newUserHandler := userhandler.NewUserHandler(newUserService)
+
+
+	routes.InitUserRoutes(router, newUserHandler)
+
+	port := fmt.Sprintf(":%s", env.Env.GoPort)
+	slog.Info(fmt.Sprintf("server running on port %s", port))
+	err = http.ListenAndServe(port, router)
+	if err != nil {
+		slog.Error("error to start server", err, slog.String("package", "main"))
+	}
+
+
 }
 
 type user struct {
