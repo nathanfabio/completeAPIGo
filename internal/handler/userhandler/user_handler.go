442a614180e1cdfdbcc10963cf2fea151b3a4254
Nathan Fabio <nathan_3_m@hotmail.com>
2024-02-06 21:23:01 -0300

    feat: add create user

--- internal/handler/userhandler/user_handler.go
+++ internal/handler/userhandler/user_handler.go
@@ -1,7 +1,41 @@
 package userhandler
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
 
-func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	"github.com/nathanfabio/completeAPIGo/internal/dto"
+	"github.com/nathanfabio/completeAPIGo/internal/handler/httperr"
+	"github.com/nathanfabio/completeAPIGo/internal/handler/validation"
+)
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	var req dto.CreateUserDto
+
+	if r.Body == http.NoBody {
+		slog.Error("body is empty", slog.String("package", "userhandler"))
+		w.WriteHeader(http.StatusBadRequest)
+		msg := httperr.NewBadRequestError("body is required")
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		slog.Error("error to decode body", "err", err, slog.String("package", "userhandler"))
+		w.WriteHeader(http.StatusBadRequest)
+		msg := httperr.NewBadRequestError("error to decode body")
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+
+	httperr := validation.ValidateHttpData(req)
+	if httperr != nil {
+		slog.Error(fmt.Sprintf("error to validate data: %v", httperr), slog.String("package", "handler_user"))
+		w.WriteHeader(httperr.Code)
+		json.NewEncoder(w).Encode(httperr)
+		return
+	}
 }
